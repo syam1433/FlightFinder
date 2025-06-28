d9aa2e71de972df84d7d69205e0acfa42867b4ea
syam1433 <126399216+syam1433@users.noreply.github.com>
2025-06-28 09:47:39 +0530

    Delete code directory

--- code/client/src/ProtectedRoute.jsx
+++
@@ -1,9 +0,0 @@
-// src/components/ProtectedRoute.jsx
-import { Navigate } from "react-router-dom";
-
-const ProtectedRoute = ({ children }) => {
-  const token = localStorage.getItem("token");
-  return token ? children : <Navigate to="/login" />;
-};
-
-export default ProtectedRoute;
